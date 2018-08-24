49a17b7e51a68cf882687e2e70c4d9de788eda10
fans656 <fans656@yahoo.com>
2018-08-24 09:20:13 +0800

    Use env variable to determine HOST and PORT
    So one can deploy more flexible

--- conf.py
+++ conf.py
@@ -4,8 +4,9 @@ import os
 debugging = os.path.exists('DEBUG')
 
 
-host = '0.0.0.0'
-port = 6561
+
+host = os.environ.get('HOST') or '0.0.0.0'
+port = int(os.environ.get('PORT', 6561))
 
 user_dir = os.path.expanduser('~')
 ssh_dir = os.path.join(user_dir, '.ssh')
